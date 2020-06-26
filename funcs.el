b24a91e2a0367fd41e540569a91f83ea1acbbd8c
GammaGames <jesse007@ymail.com>
2020-06-25 20:18:12 -0600

    visual line mode and file length category

--- funcs.el
+++ funcs.el
@@ -11,19 +11,34 @@
 (defun author-mode ()
   (when (and (stringp buffer-file-name)
     (string-match "\\.md\\'" buffer-file-name))
+      (spacemacs/toggle-highlight-currtent-line-globally-off)
       (setq markdown-header-scaling t)
       (set-frame-font "iA Writer Duospace 14" nil t)
 
       (writeroom-mode)
       (setq writeroom-width 88)
       (setq writeroom-mode-line-toggle-position 'mode-line-format)
-      (visual-line-mode)
-      (setq line-move-visual t)
+      (spacemacs/toggle-visual-line-navigation-on)
       (writeroom-toggle-mode-line)
       (wc-mode)
 
       (setq mode-line-align-left
-        '(:propertize (:eval (propertize (wc-format-modeline-string "%tw words, %w"))) face bold-italic))
+        (list
+          '(:propertize (:eval (propertize (wc-format-modeline-string "%tw words, %w - "))) face bold)
+          '(:propertize (:eval (propertize
+            (if (< (string-to-number (wc-format-modeline-string "%tw ")) 101)
+              "Micro Fiction"
+              (if (< (string-to-number (wc-format-modeline-string "%tw ")) 1000)
+                "Flash Fiction"
+                (if (< (string-to-number (wc-format-modeline-string "%tw ")) 7500)
+                  "Short Story"
+                  (if (< (string-to-number (wc-format-modeline-string "%tw ")) 20000)
+                    "Novelette"
+                      (if (< (string-to-number (wc-format-modeline-string "%tw ")) 50000)
+                        "Novella"
+                        (if (< (string-to-number (wc-format-modeline-string "%tw ")) 110000)
+                          "Novel"
+                          "Epic")))))))) face bold-italic)))
       (setq mode-line-align-middle
         '(:propertize (:eval (propertize "%b " 'face
           (let ((face (buffer-modified-p)))
